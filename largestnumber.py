87f3660128ee8345c588b112c824dcfea005f1df
santhoshraja28 <42863071+santhoshraja28@users.noreply.github.com>
2019-02-07 14:21:55 +0530

    Update largestnumber.py

--- largestnumber.py
+++ largestnumber.py
@@ -1,9 +1,9 @@
 a = int(raw_input())
 b = int(raw_input())
 c = int(raw_input())
-if(a>b) and (a>c):
+if(a>=b) and (a>c):
   largest=a
-elif(b>a) and (b>c):
+elif(b>=a) and (b>c):
   largest=b
 else:
   largest=c
