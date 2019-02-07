55d2b853860aec5a56bf6e2aa6c8ddf77f876d7f
santhoshraja28 <42863071+santhoshraja28@users.noreply.github.com>
2019-02-07 11:05:46 +0530

    Create vowelorconsonant.py

---
+++ vowelorconsonant.py
@@ -0,0 +1,7 @@
+x=str(raw_input("enter a character:"))
+if(x in('a','e','i','o','u')):
+  print("vowel")
+elif(x in('b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z')):
+  print("consonant")
+else:
+  print("invalid")    
