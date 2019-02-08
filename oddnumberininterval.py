1b91c9883ebf89c677e91ed54f4d91f39958dfec
santhoshraja28 <42863071+santhoshraja28@users.noreply.github.com>
2019-02-08 12:01:08 +0530

    Update oddnumberininterval.py

--- oddnumberininterval.py
+++ oddnumberininterval.py
@@ -1,6 +1,8 @@
 a=input().split()
 n=int(a[0])
 q=int(a[1])
+list=[]
 for i in range(n+1,q):
-    if(i%2!=0):
-        print(i,end=" ")
+  if(i%2!=0):
+   list.append(i)
+print(" ".join(map(str,list)))
